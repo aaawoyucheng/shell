35a191bad62906ea7cd6ec60f2bc1f346601a1ec
aaawoyucheng <aaa0376@163.com>
2018-11-10 23:16:14 +0800

    Update clean_eth.sh

--- clean_eth.sh
+++ clean_eth.sh
@@ -2,5 +2,5 @@ set -x
 #ip addr |grep mtu|grep -v lo|awk -F ":" '{print $2}'
 echo -n "enter ethernet card name:"
 read ethcard
-cat -e 'DEVICE="${ethcard}"\nNAME="${ethcard}"\nNM_CONTROLLED="no"\nONBOOT=yes\nTYPE=Ethernet\nBOOTPROTO=dhcp\n'>/etc/sysconfig/network-scripts/ifcfg-${ethcard}
+echo -e 'DEVICE="${ethcard}"\nNAME="${ethcard}"\nNM_CONTROLLED="no"\nONBOOT=yes\nTYPE=Ethernet\nBOOTPROTO=dhcp\n'>/etc/sysconfig/network-scripts/ifcfg-${ethcard}
 echo /etc/sysconfig/network-scripts/ifcfg-${ethcard}
